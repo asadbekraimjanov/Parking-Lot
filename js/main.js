df013368e23adca8f63008b72e1627916d1929a3
Bekhzod <stx.webdev@gmail.com>
2024-02-26 18:34:02 +0500

    Website page

---
+++ js/main.js
@@ -0,0 +1,24 @@
+document.getElementById('btn-export-1').onmouseenter = function () {
+  document.getElementById('fa-download-1').style.color = '#ffffff';
+}
+document.getElementById('btn-export-2').onmouseenter = function () {
+  document.getElementById('fa-download-2').style.color = '#ffffff';
+};
+document.getElementById('btn-export-1').onmouseleave = function () {
+  document.getElementById('fa-download-1').style.color = '#08457a';
+};
+document.getElementById('btn-export-2').onmouseleave = function () {
+  document.getElementById('fa-download-2').style.color = '#08457a';
+};
+document.getElementById('btn-export-1').onmousedown = function () {
+  document.getElementById('fa-download-1').style.color = '#08457a';
+};
+document.getElementById('btn-export-2').onmousedown = function () {
+  document.getElementById('fa-download-2').style.color = '#08457a';
+};
+document.getElementById('btn-export-1').onmouseup = function () {
+  document.getElementById('fa-download-1').style.color = '#ffffff';
+};
+document.getElementById('btn-export-2').onmouseup = function () {
+  document.getElementById('fa-download-2').style.color = '#ffffff';
+};
